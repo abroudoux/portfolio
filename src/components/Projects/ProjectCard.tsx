033319158cdcb8b2f9fc3bbf8ddd2e45831fd638
arthur <arthur.broudoux@gmail.com>
2024-01-01 17:06:58 -0800

    ProjectCards badges => map function

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -8,28 +8,25 @@ import { Badge } from "@/components/ui/badge"
 
 type ProjectCardProps = {
     title : string;
-    tags : string;
+    tags : string[];
     link : string;
 };
 
 const ProjectCard: FC<ProjectCardProps> = (props) => {
 
     const { setIsProjectCardHovered } = useStore();
-    // const controls = useAnimation();
     const controlsTopProjectCard = useAnimation();
     const controlsBottomProjectCard = useAnimation();
 
 
     const handleMouseEnter = async () => {
         setIsProjectCardHovered(true);
-        // await controls.start({ opacity: 1, y: 0 });
         await controlsTopProjectCard.start({ opacity: 1, y: 0 })
         await controlsBottomProjectCard.start({ opacity: 1, y: 0 })
     };
 
     const handleMouseLeave = async () => {
         setIsProjectCardHovered(false);
-        // await controls.start({ opacity: 0, y: -50 });
         await controlsTopProjectCard.start({ opacity: 0, y: -50 })
         await controlsBottomProjectCard.start({ opacity: 0, y: 50 })
     };
@@ -38,9 +35,11 @@ const ProjectCard: FC<ProjectCardProps> = (props) => {
         <div className={`relative w-full h-full rounded-xl hover:rounded-none border-2 border-grey-light overflow-hidden group transition-all`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity"></div>
             <a href={ props.link } className="w-full h-full flex-col-center-center px-4 py-4 relative">
-                <motion.div className="w-full h-full flex-col-start-between py-2 px-2">
+                <motion.div className="w-full h-full flex-col-start-between">
                     <motion.p animate={controlsTopProjectCard} initial={{ y: 50, opacity: 0 }} transition={{ duration: 0.1 }}>
-                        <Badge variant="outline">{ props.tags }</Badge>
+                        {props.tags.map((tag, index) => (
+                            <Badge key={index} variant="outline">{ tag }</Badge>
+                        ))}
                     </motion.p>
                     <motion.p className="text-4xl font-semibold" animate={controlsBottomProjectCard} initial={{ y: -50, opacity: 0 }} transition={{ duration: 0.3 }}>
                         { props.title }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -18,18 +18,18 @@ export default function Projects() {
             <motion.div className="w-full h-full flex-col-center-center gap-3" initial="hidden" whileInView="visible" exit={{ opacity: 0, transition: { duration: 1 } }} variants={{ visible: { transition: { staggerChildren: 0.3 } } }} viewport={{ once: true }} ref={scrollRef} style={{ overflow: "scroll" }}>
                 <div className="w-full h-2/4 py-1 flex-row-center-between gap-4">
                     <motion.div className="w-3/4 h-full" variants={itemVariants}>
-                        <ProjectCard title="test" link="/" tags="personal project" />
+                    <ProjectCard title="test" link="/" tags={["personal", "project"]} />
                     </motion.div>
                     <motion.div className="w-1/4 h-full" variants={itemVariants}>
-                        <ProjectCard title="test" link="/" tags="personal project" />
+                    <ProjectCard title="test" link="/" tags={["personal", "project"]} />
                     </motion.div>
                 </div>
                 <div className="w-full h-2/4 py-1 flex-row-center-between gap-4">
                     <motion.div className="w-1/4 h-full" variants={itemVariants}>
-                        <ProjectCard title="Wanna see more ?" link="https://github.com/abroudoux" tags="social" />
+                        <ProjectCard title="Wanna see more ?" link="https://github.com/abroudoux" tags={["social"]} />
                     </motion.div>
                     <motion.div className="w-3/4 h-full" variants={itemVariants}>
-                        <ProjectCard title="test"link="/" tags="personal project" />
+                    <ProjectCard title="test" link="/" tags={["personal", "project"]} />
                     </motion.div>
                 </div>
             </motion.div>
