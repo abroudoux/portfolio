381184e765ffd9c57fa75e3df715a9f2242eabfe
arthur <arthur.broudoux@gmail.com>
2024-01-08 16:30:23 +0100

    Projects Data

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -7,7 +7,7 @@ import ProjectCard from "@/components/Projects/ProjectCard";
 
 import ChoosifyBg from "@/assets/img/choosify-bg.png";
 import CatsAPIBg from "@/assets/img/cats-api-bg.png";
-import GithubLogo from "@/assets/img/logo-github.png"
+import GithubLogo from "@/assets/img/logo-github.png";
 
 
 export default function Projects() {
@@ -22,10 +22,10 @@ export default function Projects() {
             <motion.div className="w-full h-full flex-col-center-center gap-3" initial="hidden" whileInView="visible" exit={{ opacity: 0, transition: { duration: 1 } }} variants={{ visible: { transition: { staggerChildren: 0.3 } } }} viewport={{ once: true }} ref={scrollRef} style={{ overflow: "scroll" }}>
                 <div className="w-full h-2/4 py-1 flex-row-center-between gap-4">
                     <motion.div className="w-3/4 h-full" variants={itemVariants}>
-                        <ProjectCard title="choosify" link="/projects/choosify" tags={["personal project"]} image={ ChoosifyBg } />
+                        <ProjectCard title="choosify" link="/projects/choosify" tags={["personal project", "current"]} image={ ChoosifyBg } />
                     </motion.div>
                     <motion.div className="w-1/4 h-full" variants={itemVariants}>
-                        <ProjectCard title="cats-api" link="/projects/cats-api" tags={["personal project"]} image={ CatsAPIBg } />
+                        <ProjectCard title="cats-api" link="/projects/cats-api" tags={["personal project", "finished"]} image={ CatsAPIBg } />
                     </motion.div>
                 </div>
                 <div className="w-full h-2/4 py-1 flex-row-center-between gap-4">
