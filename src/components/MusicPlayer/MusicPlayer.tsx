9b791d7e2030550df49debb2b63860d20e1568fd
arthur <arthur.broudoux@gmail.com>
2024-02-07 13:30:01 +0100

    about sections

---
+++ src/components/MusicPlayer/MusicPlayer.tsx
@@ -0,0 +1,7 @@
+export default function MusicPlayer() {
+  return (
+    <div>
+      <h1>Music Player</h1>
+    </div>
+  );
+}
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,8 +1,13 @@
+import pp from "@/assets/img/pp.png";
+
 export default function About() {
   return (
     <section className="section flex-col-center-center" id="about">
-      <div className="w-full h-full bg-blue-400 flex-col-center-center rounded-lg">
-        <h1 className="text-9xl">About</h1>
+      <div className="w-full h-full border-2 flex-row-center-between rounded-lg">
+        <div className="h-full w-2/6 rounded-s-xl flex-col-end-center bg-red-400 p-8 gap-4">
+          <img src={pp} className="w-32 h-32 rounded-full" alt="pp" />
+        </div>
+        <div className="h-full w-4/6 rounded-e-xl flex-col-start-center bg-blue-400 p-6">test</div>
       </div>
     </section>
   );
