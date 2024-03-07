543394b6301d44c7ac541fa95a031f5dfaaf1054
arthur <arthur.broudoux@gmail.com>
2024-03-07 11:47:54 +0100

    about section - animations

--- src/lib/animations.ts
+++ src/lib/animations.ts
@@ -7,3 +7,8 @@ export const letterVariants = {
   hidden: { opacity: 0, y: 10 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.5 } }
 };
+
+export const iconsVariants = {
+  hidden: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.1 } }
+};
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,5 +1,8 @@
-import { Github, Linkedin, Mail, CircleUser } from "lucide-react";
+import { Github, Linkedin, Mail, CircleUser, icons } from "lucide-react";
 import { motion } from "framer-motion";
+import { useRef } from "react";
+
+import { itemVariants, iconsVariants } from "@/lib/animations";
 
 import ExperienceCard from "@/components/Jobs/ExperienceCard";
 import javascript from "@/assets/logos/javascript.svg";
@@ -21,25 +24,46 @@ import laravel from "@/assets/logos/laravel.svg";
 import cv from "@/assets/CV.pdf";
 
 export default function About() {
+  const scrollRef = useRef(null);
   return (
     <section className="section flex-col-center-center" id="about">
-      <motion.div className="w-full h-full border-2 flex-row-center-between rounded-lg">
-        <div className="h-full w-1/2 rounded-s-xl flex-col-end-start py-24 pl-40 gap-4">
+      <motion.div
+        className="w-full h-full border-2 flex-row-center-between rounded-lg"
+        initial="hidden"
+        whileInView="visible"
+        exit={{ opacity: 0, transition: { duration: 1 } }}
+        variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+        viewport={{ once: true }}
+        ref={scrollRef}
+        style={{ overflow: "scroll" }}>
+        <motion.div className="h-full w-1/2 rounded-s-xl flex-col-end-start py-24 pl-40 gap-4">
           <div className="flex-col-start-center w-full h-auto mb-16">
-            <h2 className="font-semibold text-7xl">Arthur Broudoux</h2>
+            <motion.h2 className="font-semibold text-7xl" variants={itemVariants}>
+              Arthur Broudoux
+            </motion.h2>
           </div>
           <div className="flex-col-start-start w-ful h-auto mb-12 text-xl">
-            <p className="pr-6">
+            <motion.p className="pr-6" variants={itemVariants}>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, dolore? Aut eaque
               accusamus autem blanditiis culpa quidem sed odio alias? Labore quae nulla totam
               necessitatibus recusandae placeat molestiae sit magni iste illo, id saepe tenetur
               explicabo sint reiciendis nemo tempora!
-            </p>
+            </motion.p>
           </div>
           <div className="w-full h-auto mt-4 mb-20">
-            <h2 className="text-4xl font-semibold py-6">Contact</h2>
-            <ul className="flex flex-col justify-center gap-2 text-xl">
-              <li>
+            <motion.h2 className="text-4xl font-semibold py-6" variants={itemVariants}>
+              Contact
+            </motion.h2>
+            <motion.ul
+              className="flex flex-col justify-center gap-2 text-xl"
+              initial="hidden"
+              whileInView="visible"
+              exit={{ opacity: 0, transition: { duration: 1 } }}
+              variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+              viewport={{ once: true }}
+              ref={scrollRef}
+              style={{ overflow: "scroll" }}>
+              <motion.li variants={itemVariants}>
                 <a
                   href="https://github.com/abroudoux"
                   className="flex flex-row items-center gap-2"
@@ -47,8 +71,8 @@ export default function About() {
                   <Github />
                   abroudoux
                 </a>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li variants={itemVariants}>
                 <a
                   href="https://linkedin.com/in/abroudoux"
                   className="flex flex-row items-center gap-2"
@@ -56,8 +80,8 @@ export default function About() {
                   <Linkedin />
                   abroudoux
                 </a>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li variants={itemVariants}>
                 <a
                   href="mailto:arthur.broudoux@gmail.com"
                   className="flex flex-row items-center gap-2"
@@ -65,67 +89,129 @@ export default function About() {
                   <Mail />
                   arthur.broudoux@gmail.com
                 </a>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li variants={itemVariants}>
                 <a href={cv} className="flex flex-row items-center gap-2" target="_blank">
                   <CircleUser />
                   Curiculum Vitae
                 </a>
-              </li>
-            </ul>
+              </motion.li>
+            </motion.ul>
           </div>
-          <div className="w-full h-1/4 flex-col-start-center">
+          <motion.div
+            className="w-full h-1/4 flex-col-start-center"
+            initial="hidden"
+            whileInView="visible"
+            exit={{ opacity: 0, transition: { duration: 1 } }}
+            variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+            viewport={{ once: true }}
+            ref={scrollRef}
+            style={{ overflow: "scroll" }}>
             <div className="grid grid-cols-8 gap-4">
-              <img
+              <motion.img
+                variants={iconsVariants}
                 src={javascript}
                 className="w-16 h-16 bg-white/5 p-3 rounded-lg"
                 alt="logo-javascript"
               />
-              <img
+              <motion.img
+                variants={iconsVariants}
                 src={typescript}
                 className="w-16 h-16 bg-white/5 p-3 rounded-lg"
                 alt="logo-typescript"
               />
-              <img src={scss} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-scss" />
-              <img src={react} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-react" />
-              <img
+              <motion.img
+                variants={iconsVariants}
+                src={scss}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-scss"
+              />
+              <motion.img
+                variants={iconsVariants}
+                src={react}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-react"
+              />
+              <motion.img
+                variants={iconsVariants}
                 src={tailwind}
                 className="w-16 h-16 bg-white/5 p-3 rounded-lg"
                 alt="logo-tailwindcss"
               />
-              <img src={nestjs} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-nestjs" />
-              <img src={git} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-git" />
-              <img src={bash} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-bash" />
-              <img src={nodejs} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-nodejs" />
-              <img
+              <motion.img
+                variants={iconsVariants}
+                src={nestjs}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-nestjs"
+              />
+              <motion.img
+                variants={iconsVariants}
+                src={git}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-git"
+              />
+              <motion.img
+                variants={iconsVariants}
+                src={bash}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-bash"
+              />
+              <motion.img
+                variants={iconsVariants}
+                src={nodejs}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-nodejs"
+              />
+              <motion.img
+                variants={iconsVariants}
                 src={mongodb}
                 className="w-16 h-16 bg-white/5 p-3 rounded-lg"
                 alt="logo-mongodb"
               />
-              <img src={rust} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-rust" />
-              <img src={python} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-python" />
-              <img
+              <motion.img
+                variants={iconsVariants}
+                src={rust}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-rust"
+              />
+              <motion.img
+                variants={iconsVariants}
+                src={python}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-python"
+              />
+              <motion.img
+                variants={iconsVariants}
                 src={postman}
                 className="w-16 h-16 bg-white/5 p-3 rounded-lg"
                 alt="logo-postman"
               />
-              <img src={docker} className="w-16 h-16 bg-white/5 p-3 rounded-lg" alt="logo-docker" />
-              <img
+              <motion.img
+                variants={iconsVariants}
+                src={docker}
+                className="w-16 h-16 bg-white/5 p-3 rounded-lg"
+                alt="logo-docker"
+              />
+              <motion.img
+                variants={iconsVariants}
                 src={postgresql}
                 className="w-16 h-16 bg-white/5 p-3 rounded-lg"
                 alt="logo-postgresql"
               />
-              <img
+              <motion.img
+                variants={iconsVariants}
                 src={laravel}
                 className="w-16 h-16 bg-white/5 p-3 rounded-lg"
                 alt="logo-laravel"
               />
             </div>
-          </div>
-        </div>
-        <div className="h-full w-1/2 rounded-e-xl flex-col-start-start py-24 px-20">
-          <div className="w-full h-full flex-col-start-start mb-4">
-            <h2 className="text-4xl font-semibold mb-8 italic">Professional Experiences</h2>
+          </motion.div>
+        </motion.div>
+        <motion.div className="h-full w-1/2 rounded-e-xl flex-col-start-start py-24 px-20">
+          <motion.div className="w-full h-full flex-col-start-start mb-4" variants={itemVariants}>
+            <motion.h2 className="text-4xl font-semibold mb-8 italic" variants={itemVariants}>
+              Professional Experiences
+            </motion.h2>
             <ExperienceCard
               post="Internship web developper"
               name="Coquille Consulting"
@@ -140,9 +226,11 @@ export default function About() {
               description="My role as a web developper intern was to develop the company's website. I was also in charge of the development of a specific website for the company which was used for the company's internal use during meetings and presentations."
               technos={["React", "SCSS", "TypeScript"]}
             />
-          </div>
-          <div className="w-full h-full flex-col-start-start">
-            <h2 className="text-4xl font-semibold mb-8 italic">Education</h2>
+          </motion.div>
+          <motion.div className="w-full h-full flex-col-start-start" variants={itemVariants}>
+            <motion.h2 className="text-4xl font-semibold mb-8 italic" variants={itemVariants}>
+              Education
+            </motion.h2>
             <ExperienceCard
               post="B2 - Web Developper"
               name="My Digital School Angers - Web and Multimedia cycle"
@@ -157,8 +245,8 @@ export default function About() {
               description="Admitted well mention"
               technos={["English", "SES", "Mathematics"]}
             />
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </motion.div>
     </section>
   );
