95d2df44f12acacfce2f8191932f6aa8c5969cd6
arthur <arthur.broudoux@gmail.com>
2024-04-20 23:22:14 +0200

    refractor: update dependencies & project infos

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { Toaster } from "sonner";
 
 import { ThemeProvider } from "@/config/theme-provider";
 import router from "@/config/router";
@@ -10,6 +9,5 @@ import "@/style/layouts.css";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <RouterProvider router={router} />
-    <Toaster richColors />
   </ThemeProvider>
 );
