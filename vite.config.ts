18b1f51ed31d0751874d1948dec6d17309f52d8b
arthur <arthur.broudoux@gmail.com>
2024-04-29 13:24:47 +0200

    dep: fix bug deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-refresh";
 import svgrPlugin from "vite-plugin-svgr";
 import envCompatible from "vite-plugin-env-compatible";
 import path from "path";
